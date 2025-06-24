365c1b0df47b2290740a0e8071b9bccae6c0ff09
divesh <rajpootdivesh5@gmail.com>
2025-06-24 16:13:06 +0530

    feat: add static html files and fix hello handler path
    - Add index.html and form.html static files for web server
    - Fix hello handler path to include leading slash
    - Update static file server path and change port to 8081

--- simple-server/app/handler/helloHandler.go
+++ simple-server/app/handler/helloHandler.go
@@ -6,7 +6,7 @@ import (
 )
 
 func HelloHandler(w http.ResponseWriter, r *http.Request) {
-	if r.URL.Path != "hello" {
+	if r.URL.Path != "/hello" {
 		http.Error(w, "404 not found", http.StatusNotFound)
 		return
 	}
--- simple-server/app/main.go
+++ simple-server/app/main.go
@@ -8,16 +8,13 @@ import (
 )
 
 func main() {
-	fileserver := http.FileServer(http.Dir("../static"))
-
+	fileserver := http.FileServer(http.Dir("./static"))
 	http.Handle("/", fileserver)
 	http.HandleFunc("/hello", handler.HelloHandler)
 	http.HandleFunc("/form", handler.FormHandler)
 
-	fmt.Println("server running on http://localhost:8080")
-
-	if err := http.ListenAndServe("8080", nil); err != nil {
+	fmt.Printf("port running on http://localhost:8081/\n")
+	if err := http.ListenAndServe(":8081", nil); err != nil {
 		log.Fatal(err)
 	}
-
 }
