ede0bb331c47fa2441342b7ce8c0cca7bede9aef
divesh <rajpootdivesh5@gmail.com>
2025-07-09 14:38:11 +0530

    feat: initialize go-auth project with models and database setup
    Add initial project structure with user and claims models
    Set up database configuration and dependencies

---
+++ go-auth-new/go.mod
@@ -0,0 +1,11 @@
+module go-auth
+
+go 1.24.3
+
+require (
+	github.com/golang-jwt/jwt v3.2.2+incompatible // indirect
+	github.com/jinzhu/inflection v1.0.0 // indirect
+	github.com/jinzhu/now v1.1.5 // indirect
+	golang.org/x/text v0.20.0 // indirect
+	gorm.io/gorm v1.30.0 // indirect
+)
---
+++ go-auth-new/models/index.go
@@ -0,0 +1,23 @@
+package models
+
+import (
+	"fmt"
+
+	"gorm.io/gorm"
+)
+
+type Config struct {
+	Host     string
+	Port     string
+	User     string
+	Password string
+	DBName   string
+	SSLMode  string
+}
+
+var DB *gorm.DB
+
+func InitDB() {
+	dsn := fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=%s")
+	fmt.Println(dsn)
+}
